f07e1b4977bd99fe3c009a4350b6aaf70ff78112
Konstiantyn <kniziienko10@gmail.com>
2021-09-08 13:46:07 +0300

    test header scroll

--- #src/js/header.js
+++ #src/js/header.js
@@ -1,26 +1,9 @@
 $(document).ready(function () {
   $(".header__burger").click(function (event) {
     $(".header__burger,.header__menu").toggleClass("active");
-    $("body").toggleClass("lock");
   });
 
   $(".header__list").click(function (event) {
     $(".header__burger,.header__menu").removeClass("active");
-    $("body").removeClass("lock");
   });
 });
-
-jQuery(function ($) {
-   $(‘a[href*=”#”]:not([href=”#”])’).click(function() {
-   if (location.pathname.replace(/^\//,”) == this.pathname.replace(/^\//,”) && location.hostname == this.hostname) {
-   var target = $(this.hash);
-   target = target.length ? target : $(‘[name=’ + this.hash.slice(1) +’]’);
-   if (target.length) {
-   $(‘html, body’).animate({
-   scrollTop: target.offset().top
-   }, 1000);
-   return false;
-   }
-   }
-   });
-   });
