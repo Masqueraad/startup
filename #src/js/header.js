b57e5e17aa3e290978ab368e4a4b9912d91f0152
Konstiantyn <kniziienko10@gmail.com>
2021-09-09 15:33:18 +0300

    fixed bugs

--- #src/js/header.js
+++ #src/js/header.js
@@ -3,8 +3,7 @@ $(document).ready(function () {
     $(".header__burger,.header__menu").toggleClass("active");
     $("body").toggleClass("lock");
   });
-
-  $(".header__list").click(function (event) {
+  $(".header__list, .header__logo").click(function (event) {
     $(".header__burger,.header__menu").removeClass("active");
     $("body").removeClass("lock");
   });
